b93e73db8af814fab5cbe122f90914a03895bf14
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-01-10 19:38:49 -0300

    user.model desenvolvido com token + bcrypt

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { Schema } = mongoose;
+
+const userShema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true
+	},
+	password: {
+		type: String,
+		required: true,
+		select: false
+	},
+	address: {
+		type: String,
+		required: true
+	},
+	phone: {
+		type: String,
+		required: true
+	},
+	adopt: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Pet'
+	},
+	adoptionOffers: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Pet'
+		}
+	]
+});
+
+userShema.pre('save', async function (next){
+	const hash = await bcrypt.hash(this.password, 10);
+	this.password = hash;
+
+	next();
+});
+
+userShema.methods = {
+	checkPassword: function (password) {
+		return bcrypt.compare(password, this.password);
+	},
+	generateToken: function () {
+		return jwt.sign(
+			{ id: this._id },
+			process.env.APP_SECRET, {
+				expiresIn: 86400,
+			});
+	}
+};
+
+const User = mongoose.model('User', userShema);
+
+module.exports = User;
