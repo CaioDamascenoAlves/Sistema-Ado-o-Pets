674255bf5663095c1b8ff316e4e2ee526f24afc7
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-01-10 19:22:08 -0300

    Desenvolvido Teste do Model User

---
+++ backend/src/tests/user.model.test.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/user.model');
+
+describe('User model', () => {
+	it('should hash the password before saving', async () => {
+		const user = new User({
+			name: 'Caio Damasceno',
+			email: 'caio@exemplo.com',
+			password: '123456',
+			adress: 'João Monlevade',
+			phone: '+1234567890',
+		});
+
+		await user.save();
+		expect(await bcrtpt.compare('123456', user.password)).toBe(true);
+	});
+
+	it('should validate the password', async () => {
+		const user = new User({
+			name: 'Caio Damasceno',
+			email: 'caio@exemplo.com',
+			password: '123456',
+			adress: 'João Monlevade',
+			phone: '+1234567890',
+		});
+		
+		await user.save();
+		expect(user.generateToken()).toBeDefined();
+	});
+});
